91785df7bc8cad07c3e32984f5473bb6026e8238
Matheus Silva <126895722+0xcROW@users.noreply.github.com>
2024-10-03 18:38:15 -0300

    finished the functions create, list, read, write, delete

--- index.js
+++ index.js
@@ -23,35 +23,46 @@ async function main() {
 
         const choice = readLineSync.question("Pick a choice: ");
 
-        switch(choice) {
-            case '1':
-                const fileName = readLineSync.question("Type the file name: ");
-                const fileContent = readLineSync.question("Type the content or press enter to ignore: ");
-                const createFilePath = path.join(baseDir, fileName)
-                const fileMessage = await fileManager.createFile(createFilePath, fileContent);
-                console.log(fileMessage);
-                break;
-            case '2':
-                const fileList = await fileManager.listFiles(baseDir);
-                console.log("Files in the directory: ", fileList)
-                break;
-            case '3':
-                const fileRead = readLineSync.question("What is the file name? " );
-                const fileReadPath = path.join(baseDir, fileRead);
-                const fileReadContent = await fileManager.readFile(fileReadPath);
-                console.log("File content: \n\n", fileReadContent);
-                break;
-            case '4':
-                //5:16 Try e Catch video
-                console.log("4");
-                break;
-            case '5':
-                break;
-            case '6':
-                console.log("\nLeaving... au revoir!\n");
-                return;
-            default:
-                console.error("Unknown option, try again.")
+        try {
+            switch(choice) {
+                case '1':
+                    const fileName = readLineSync.question("Type the file name: ");
+                    const fileContent = readLineSync.question("Type the content or press enter to ignore: ");
+                    const createFilePath = path.join(baseDir, fileName)
+                    const fileMessage = await fileManager.createFile(createFilePath, fileContent);
+                    console.log(fileMessage);
+                    break;
+                case '2':
+                    const fileList = await fileManager.listFiles(baseDir);
+                    console.log("Files in the directory: ", fileList)
+                    break;
+                case '3':
+                    const fileRead = readLineSync.question("What is the file name? " );
+                    const fileReadPath = path.join(baseDir, fileRead);
+                    const fileReadContent = await fileManager.readFile(fileReadPath);
+                    console.log("File content: \n\n", fileReadContent);
+                    break;
+                case '4':
+                    const fileWrite = readLineSync.question("Type the file name: ");
+                    const fileWritePath = path.join(baseDir, fileWrite);
+                    const fileWriteNewContent = readLineSync.question("Type the content to be saved: ");
+                    const fileWriteMessage = await fileManager.writeFile(fileWritePath, fileWriteNewContent);
+                    console.log(fileWriteMessage)
+                    break;
+                case '5':
+                    const fileDeleteName = readLineSync.question("What is the file name to be deleted? ");
+                    const fileDeletePath = path.join(baseDir, fileDeleteName);
+                    const fileDeleteMessage = await fileManager.deleteFile(fileDeletePath);
+                    console.log(fileDeleteMessage);
+                    break;
+                case '6':
+                    console.log("\nLeaving... au revoir!\n");
+                    return;
+                default:
+                    console.error("Unknown option, try again.")
+            }
+        } catch (err) {
+            console.log(err);
         }
     }
 }
