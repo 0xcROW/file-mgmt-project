7ff4b19d6d7c537e7ea00ee5af08d8be90df2886
Matheus Silva <126895722+0xcROW@users.noreply.github.com>
2024-10-03 00:46:12 -0300

    Almost finishing the project

--- fileManager.js
+++ fileManager.js
@@ -25,4 +25,52 @@ function createFile(filePath, content = '') {
     })
 }
 
-module.exports = {createDirectory, createFile}
+function listFiles(dirPath) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(dirPath, (err, files) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(files)
+            }
+        });
+    });
+}
+
+function readFile(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+function writeFile(filePath, content) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filePath, content, 'utf-8', (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve('File written successfully!');
+            }
+        });
+    });
+}
+
+function deleteFile(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve('File deleted successfully!')
+            }
+        });
+    });
+}
+
+module.exports = {createDirectory, createFile, listFiles, readFile, writeFile, deleteFile}
--- index.js
+++ index.js
@@ -1,54 +1,58 @@
 const fileManager = require('./fileManager');
 const readLineSync = require('readline-sync');
 const path = require('path');
+const { read } = require('fs');
 
 
 
-function main() {
+async function main() {
 
     const baseDir = path.join(__dirname, "my_files");
+
+    fileManager.createDirectory(baseDir);
 
     while(true) {
 
         console.log("\nMenu:");
-        console.log("1 - Criar arquivo");
-        console.log("2 - Listar arquivos");
-        console.log("3 - Ler arquivo");
-        console.log("4 - Escrever arquivo");
-        console.log("5 - Deletar arquivo");
-        console.log("6 - Sair");
+        console.log("1 - Create file");
+        console.log("2 - List files");
+        console.log("3 - Read file");
+        console.log("4 - Write file");
+        console.log("5 - Delete file");
+        console.log("6 - Quit");
 
-        const choice = readLineSync.question("Escolha uma opção: ");
+        const choice = readLineSync.question("Pick a choice: ");
 
         switch(choice) {
             case '1':
-                const fileName = readLineSync.question("Digite o nome do arquivo: ");
-                const fileContent = readLineSync.question("Digite o conteúdo ou tecle enter para ignorar: ");
-
-                const createFilePath = path.join(__dirname, fileName)
-
-                const fileMessage = fileManager.createFile(createFilePath, fileContent);
-
-
+                const fileName = readLineSync.question("Type the file name: ");
+                const fileContent = readLineSync.question("Type the content or press enter to ignore: ");
+                const createFilePath = path.join(baseDir, fileName)
+                const fileMessage = await fileManager.createFile(createFilePath, fileContent);
                 console.log(fileMessage);
                 break;
             case '2':
-                console.log("2");
+                const fileList = await fileManager.listFiles(baseDir);
+                console.log("Files in the directory: ", fileList)
                 break;
             case '3':
-                console.log("3");
+                const fileRead = readLineSync.question("What is the file name?" );
+                const fileReadPath = path.join(baseDir, fileRead);
+                const fileReadContent = await fileManager.readFile(fileReadPath);
+                console.log("File content: ", fileReadContent);
                 break;
             case '4':
+                //5:16 Try e Catch video
                 console.log("4");
                 break;
             case '5':
                 console.log("5");
                 break;
             case '6':
-                console.log("Saindo... tchau!");
+                console.log("Leaving... au revoir!");
                 return;
             default:
-                console.error("Opção inválida, tente novamente.")
+                console.error("Unknown option, try again.")
         }
     }
 }
